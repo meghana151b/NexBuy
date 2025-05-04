5398509faec2d990af7be0783d30af7594b9c55e
meghana151b <b11meghana@gmail.com>
2025-05-04 23:50:00 +0530

    RegistrationService

---
+++ src/main/java/com/registrationservice/RegistrationServiceeApplication.java
@@ -0,0 +1,13 @@
+package com.registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceeApplication.class, args);
+	}
+
+}
